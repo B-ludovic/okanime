c473c96a8e3652a2720719d3d11281049e316ce1
B-ludovic <timboo974@gmail.com>
2025-11-29 18:43:52 +0100

    Feat: ajout des routes d'authentification

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,17 +1,16 @@
 import express from 'express';
+import { register } from 'module';
+import { login } from '../controllers/authController';
+import { register, getMe, getMe } from '../controllers/authController.js';
+import { authMiddleware } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 // Route publique - inscription
-router.post('/register', (req, res) => {
-  res.json({ message: 'Inscription' });
-  
-});
+router.post('/register', register );
+router.post('/login', login);
 
-// Route publique - connexion
-router.post('/login', (req, res) => {
-  res.json({ message: 'Connexion' });
-  
-});
+// Route protégée - profil utilisateur
+router.get('/me', authMiddleware, getMe );
 
 export default router;
