53c4927941ab79be84b23e2cabb0c2aa2c2b5f31
B-ludovic <timboo974@gmail.com>
2025-11-29 21:22:49 +0100

    Refactor: mise à jour du layout pour CSS pur

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="fr" data-theme="okanime">
+    <html lang="fr">
       <body className={inter.className}>
         {children}
       </body>
