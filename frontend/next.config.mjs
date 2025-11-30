9a05e912be754dcd1e1fc974f6907d1414a240f6
B-ludovic <timboo974@gmail.com>
2025-11-30 20:26:25 +0100

    feat: configuration pour déploiement Render

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,24 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'cdn.myanimelist.net',
-        pathname: '/images/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'ui-avatars.com',
-        pathname: '/api/**',
-      },
-      {
-        protocol: 'https',
         hostname: 'res.cloudinary.com',
-        pathname: '/**',
       },
     ],
   },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
